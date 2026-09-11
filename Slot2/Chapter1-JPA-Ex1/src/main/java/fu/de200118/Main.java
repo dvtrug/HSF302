60c480826f302cacdcdeb69ec05556fb0b59f828
Do Trung <thanhtrungxxz@gmail.com>
2026-09-11 15:08:55 +0700

    feat(TODO 0.2): database connect

--- Slot2/Chapter1-JPA-Ex1/src/main/java/fu/de200118/Main.java
+++ Slot2/Chapter1-JPA-Ex1/src/main/java/fu/de200118/Main.java
@@ -5,3 +5,9 @@ import jakarta.persistence.Persistence;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("HSF302_Chapter1");
+        System.out.println("EMF tao thanh cong!");
+    }
+}
