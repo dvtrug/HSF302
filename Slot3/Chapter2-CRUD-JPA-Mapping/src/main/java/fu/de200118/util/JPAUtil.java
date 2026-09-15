895fa4362b0f5b7a9318194afb6fcf18c1433e93
Do Trung <thanhtrungxxz@gmail.com>
2026-09-15 16:46:14 +0700

    feat(TODO 2.5): create EntityManagerFactory in JPAUtil

--- Slot3/Chapter2-CRUD-JPA-Mapping/src/main/java/fu/de200118/util/JPAUtil.java
+++ Slot3/Chapter2-CRUD-JPA-Mapping/src/main/java/fu/de200118/util/JPAUtil.java
@@ -1,4 +1,22 @@
 package fu.de200118.util;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class JPAUtil {
+    private static final EntityManagerFactory EMF =
+            Persistence.createEntityManagerFactory("HSF302_Chapter1");
+
+    private JPAUtil() {
+    }
+
+    public static EntityManagerFactory getEMF() {
+        return EMF;
+    }
+
+    public static void closeEMF() {
+        if (EMF.isOpen()) {
+            EMF.close();
+        }
+    }
 }
